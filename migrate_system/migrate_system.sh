281c54f453b1851c386e6d50c99ebf329f440eb6
nulysses <adr.zivil@gmail.com>
2021-07-08 08:12:16 +0200

    1

--- migrate_system/migrate_system.sh
+++ migrate_system/migrate_system.sh
@@ -1,6 +1,59 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# ar18
 
-# Run this script in the live system after installation to migrate
+# Prepare script environment
+{
+  # Script template version 2021-07-06_08:05:30
+  # Make sure some modification to LD_PRELOAD will not alter the result or outcome in any way
+  LD_PRELOAD_old="${LD_PRELOAD}"
+  LD_PRELOAD=
+  # Determine the full path of the directory this script is in
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+  script_path="${script_dir}/$(basename "${0}")"
+  #Set PS4 for easier debugging
+  export PS4='\e[35m${BASH_SOURCE[0]}:${LINENO}: \e[39m'
+  # Determine if this script was sourced or is the parent script
+  if [ ! -v ar18_sourced_map ]; then
+    declare -A -g ar18_sourced_map
+  fi
+  if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    ar18_sourced_map["${script_path}"]=1
+  else
+    ar18_sourced_map["${script_path}"]=0
+  fi
+  # Initialise exit code
+  if [ ! -v ar18_exit_map ]; then
+    declare -A -g ar18_exit_map
+  fi
+  ar18_exit_map["${script_path}"]=0
+  # Save PWD
+  if [ ! -v ar18_pwd_map ]; then
+    declare -A -g ar18_pwd_map
+  fi
+  ar18_pwd_map["${script_path}"]="${PWD}"
+  # Get old shell option values to restore later
+  shopt -s inherit_errexit
+  IFS=$'\n' shell_options=($(shopt -op))
+  # Set shell options for this script
+  set -o pipefail
+  set -eu
+  if [ ! -v ar18_parent_process ]; then
+    export ar18_parent_process="$$"
+  fi
+  # Get import module
+  if [ ! -v ar18.script.import ]; then
+    mkdir -p "/tmp/${ar18_parent_process}"
+    cd "/tmp/${ar18_parent_process}"
+    curl -O https://raw.githubusercontent.com/ar18-linux/ar18_lib_bash/master/ar18_lib_bash/script/import.sh > /dev/null 2>&1 && . "/tmp/${ar18_parent_process}/import.sh"
+    cd "${ar18_pwd_map["${script_path}"]}"
+  fi
+}
+#################################SCRIPT_START##################################
+
+ar18.script.import ar18.script.execute_with_sudo
+ar18.script.import ar18.script.obtain_sudo_password
+
+ar18.script.obtain_sudo_password
 
 user_name="nulysses"
 user_id="12345"
@@ -11,17 +64,38 @@ target_uuid="43751c3c-019c-4d6f-a92c-2b654fa7b824"
 read -p "secret? " secret
 
 # Close luks used by installer
-cryptsetup close "luks-${target_uuid}"
+ar18.script.execute_with_sudo cryptsetup close "luks-${target_uuid}"
 
-echo "${secret}" | cryptsetup open "/dev/disk/by-uuid/${target_uuid}" new_system
-mkdir -p /mnt/new_system
-mount "/dev/mapper/new_system" "/mnt/new_system"
+echo "${secret}" | ar18.script.execute_with_sudo cryptsetup open "/dev/disk/by-uuid/${target_uuid}" "new_system"
+ar18.script.execute_with_sudo mkdir -p "/mnt/new_system"
+ar18.script.execute_with_sudo mount "/dev/mapper/new_system" "/mnt/new_system"
 
 
-echo "${secret}" | cryptsetup open "/dev/disk/by-uuid/${source_uuid}" old_system
-mkdir -p /mnt/old_system
-mount "/dev/mapper/old_system" "/mnt/old_system"
+echo "${secret}" | ar18.script.execute_with_sudo cryptsetup open "/dev/disk/by-uuid/${source_uuid}" "old_system"
+ar18.script.execute_with_sudo mkdir -p /mnt/old_system
+ar18.script.execute_with_sudo mount "/dev/mapper/old_system" "/mnt/old_system"
 
 chroot "/mnt/new_system" usermod -u "${user_id}" "${user_name}"
 chroot "/mnt/new_system" groupmod -g "${group_id}" "${user_name}"
 
+##################################SCRIPT_END###################################
+# Restore environment
+{
+  # Restore old shell values
+  set +x
+  for option in "${shell_options[@]}"; do
+    eval "${option}"
+  done
+  # Restore LD_PRELOAD
+  LD_PRELOAD="${LD_PRELOAD_old}"
+  # Restore PWD
+  cd "${ar18_pwd_map["${script_path}"]}"
+}
+# Return or exit depending on whether the script was sourced or not
+{
+  if [ "${ar18_sourced_map["${script_path}"]}" = "1" ]; then
+    return "${ar18_exit_map["${script_path}"]}"
+  else
+    exit "${ar18_exit_map["${script_path}"]}"
+  fi
+}
